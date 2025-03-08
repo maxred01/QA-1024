8f26432abf0e96a2ffbd1b511953703cbbdf70fd
jara1991 <porabkovicaroslav@gmail.com>
2025-03-09 00:38:03 +0300

    training

---
+++ Jaroslaw_porabkovich/pj_1.py
@@ -0,0 +1,13 @@
+def calculate_area(shape, a, b):
+    if shape == 'прямоугольник':
+        res = a * b
+        print(f"Площадь прямоугольника: {res}")
+    else:
+        print('Ошибка: неизвестная фигура')
+
+
+# Ввод данных
+shape = input('Введите фигуру: ')
+side_a = float(input('Длина стороны "a": '))
+side_b = float(input('Длина стороны "b": '))
+calculate_area(shape,side_a,side_b)
