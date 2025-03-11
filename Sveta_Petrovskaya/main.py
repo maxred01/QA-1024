75c931870b3ac46d2336e88752f25c363dcb026f
Petrovskaya-Svetlana <s7606003@mail.ru>
2025-03-11 23:01:33 +0300

    homework_2 done

---
+++ Sveta_Petrovskaya/main.py
@@ -0,0 +1,20 @@
+#import requests
+
+#response = requests.post('http://httpbin.org/post', data={'key': 'value'})
+
+#response_data = response.json()
+
+#print(response.text)
+
+#assert response_data['headers']['Host'] == 'httpbin.org'
+
+#assert response.url == 'http://httpbin.org/post'
+
+import requests  # Не забудьте импортировать библиотеку requests
+
+
+
+
+
+
+
