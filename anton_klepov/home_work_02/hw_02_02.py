a54c4f86ee30ad5e4a2df4c50ca5329a25dd0dda
hakeluk <hakeluk01@yandex.ru>
2025-03-16 21:21:58 +0300

    Уровень 2: Средний

---
+++ anton_klepov/home_work_02/hw_02_02.py
@@ -0,0 +1,19 @@
+from pickle import FALSE
+
+import requests
+
+url = 'https://httpbin.org/post'
+data = {
+    'username': 'test_user',
+    'password': 'qwerty'
+}
+response = requests.post(url, data)
+if response.status_code == 200:
+    print(response.json()['form'])
+elif response.status_code != 200 and response.status_code < 400:
+    print(f'Ошибка: {response.status_code}')
+try:
+    response = requests.post(url, data)
+    response.raise_for_status()
+except requests.exceptions.HTTPError as err:
+    print(f"Ошибка: {err}")
