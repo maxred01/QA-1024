bc345bce988179434a63ccaa2c36665f4ad7894f
Alesia <3344936@mail.ru>
2025-03-18 22:07:13 +0300

    2

--- alesia_nikitsina/home_work2/home_work2.py
+++ alesia_nikitsina/home_work2/home_work2.py
@@ -1,5 +1,5 @@
 import requests
-response=requests.post('https://httpbin.org/json')
+response=requests.get('https://httpbin.org/json')
 data=response.json
 response.status_code==200,f'ожидался ответ 200'
 assert 'slideshow' in data
