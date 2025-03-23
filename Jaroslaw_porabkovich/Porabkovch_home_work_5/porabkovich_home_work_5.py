2a29698974ce3bd5716ed789b24ffa17c1a58301
jara1991 <porabkovicaroslav@gmail.com>
2025-03-23 23:28:42 +0300

    done

--- Jaroslaw_porabkovich/Porabkovch_home_work_5/porabkovich_home_work_5.py
+++ Jaroslaw_porabkovich/Porabkovch_home_work_5/porabkovich_home_work_5.py
@@ -2,21 +2,26 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-
-# Укажите путь к драйверу (пример для Windows)
 driver = webdriver.Chrome()
 
-# Открыть страницу
 driver.get("https://demoqa.com/buttons")
 driver.maximize_window()
 button_double = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div[2]/div[1]/button')
 actions = ActionChains(driver)
 actions.double_click(button_double).perform()
+doubleClick_message = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div[2]/p[1]')
+assert doubleClick_message.text == 'You have done a double click'
 time.sleep(5)
 button_right_click = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div[2]/div[2]/button')
+scroll = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div[3]')
+actions.scroll_to_element(scroll).perform()
 actions.context_click(button_right_click).perform()
+rightClick_message = driver.find_element(By.XPATH, '//*[@id="rightClickMessage"]')
+assert rightClick_message.text == 'You have done a right click'
 time.sleep(5)
 button_single_click = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div[2]/div[3]/button')
 button_single_click.click()
+oneClick_message = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div[2]/p[3]')
+assert oneClick_message.text == 'You have done a dynamic click'
 time.sleep(5)
 driver.quit()
