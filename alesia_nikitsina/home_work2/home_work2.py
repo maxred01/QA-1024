7bc7e0ff100ec1bfd449201a361bdfce9d825714
Alesia <3344936@mail.ru>
2025-03-23 15:05:57 +0300

    home work2

--- alesia_nikitsina/home_work2/home_work2.py
+++ alesia_nikitsina/home_work2/home_work2.py
@@ -1,8 +1,8 @@
 import requests
 response=requests.get('https://httpbin.org/json')
 data=response.json
-response.status_code==200,f'ожидался ответ 200'
-assert 'slideshow' in data
-var = {'title'} in {'slideshow'}
+var = response.status_code == 200, (f'ожидался ответ 200')
+assert ('slideshow')in data
+data= {'title'} in {'slideshow'}
 title='slideshow','title'
 print(f'Title:{'title'}')
