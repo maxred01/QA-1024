39ebff68873816ed92d847350e01692b42127cc9
silentden671 <dimapahtusov77@gmail.com>
2025-03-24 21:49:01 +0300

    fifth

---
+++ dima_pahtusov/classwork06/selenium02.py
@@ -0,0 +1,23 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+# Укажите путь к драйверу (пример для Windows)
+driver = webdriver.Chrome()
+
+# Открыть страницу
+driver.get("https://demoqa.com/checkbox")
+driver.maximize_window()
+time.sleep(2)
+
+driver.find_element(By.XPATH, '//button[@class="rct-collapse rct-collapse-btn"]').click()
+driver.find_element(By.XPATH, '//span[@class="rct-checkbox"]').click()
+
+
+
+time.sleep(5)
+
+
+# Закрыть браузер
+driver.quit()
