2158deea26bc4c57dabde65b6cfa24cfa9845517
jara1991 <porabkovicaroslav@gmail.com>
2025-05-05 00:37:55 +0300

    я хз

--- Jaroslaw_porabkovich/Home_work_6/homw_work_6.py
+++ Jaroslaw_porabkovich/Home_work_6/homw_work_6.py
@@ -1,33 +1,41 @@
-import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-
-
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import time
 driver = webdriver.Chrome()
 
 driver.get("https://demoqa.com/radio-button")
 driver.maximize_window()
 # scrolls = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div[3]/div/div/iframe")
-loc_yes = driver.find_element(By.CSS_SELECTOR, "label[for='yesRadio']")
+loc_yes = driver.find_element(By.ID, "yesRadio")
 loc_impressive = driver.find_element(By.CSS_SELECTOR, "label[for='impressiveRadio']")
 loc_no = driver.find_element(By.CSS_SELECTOR, "label[for='noRadio']")
-# message_yes = driver.find_element(By.XPATH, "//span[contains(text(),'Yes')]").text
+# text_message_yes = driver.find_element(By.XPATH, "//span[@class='text-success']")
 # message_no = driver.find_element(By.XPATH, "//span[contains(text(),'Impressive')]").text
 # messages = [message_yes,message_no]
-locators = [loc_yes,loc_impressive,loc_no]
-
-# ansvers = ['Yes','Impressive']
-
-for loc in locators:
-    ActionChains(driver).move_to_element(loc).click().perform()
-    time.sleep(3)
+# locators = [loc_yes,loc_impressive,loc_no]
+#
+# # ansvers = ['Yes','Impressive']
+#
+# for loc in locators:
+#     ActionChains(driver).move_to_element(loc).click().perform()
+#     time.sleep(3)
 #     loc.click()
 #     # time.sleep(3)
 #     # assert f'{ansvers}' in f'{messages}'
 
-# ActionChains(driver).move_to_element(loc_yes).click().perform()
-# ActionChains(driver).move_to_element(loc_yes).click().perform()
-# ActionChains(driver).move_to_element(loc_yes).click().perform()
 
+ActionChains(driver).move_to_element(loc_yes).click()
+# time.sleep(3)
+# text_message_yes = driver.find_element(By.XPATH, "//span[@class='text-success']")
+# # text_message_yes = WebDriverWait(driver, 10).until(
+# #         EC.presence_of_element_located((By.XPATH, "//span[contains(@class, 'text-success')]"))
+# # )
+# assert text_message_yes.text == 'yes'
+ActionChains(driver).move_to_element(loc_impressive).click().perform()
+ActionChains(driver).move_to_element(loc_no).click().perform()
+# loc_yes.click()
+# assert
+# ActionChains(driver).move_to_element(loc_yes).click().perform()
